33a149b9ef4ca3017b3ced382065fabb8eee909c
Johan Vargas Sanchez <pvargass2@soy.sena.edu.co>
2025-12-10 13:49:19 -0500

    Implementation of SASS, change to scss files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // src/App.tsx
 import HomePage from './pages/Homepage';
-import './App.css';
+import './App.scss';
 
 function App() {
   return <HomePage />;
--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { ProductCardProps } from '../../types';
-import './styles.css';
+import './styles.scss';
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const handleAddToCart = () => {
